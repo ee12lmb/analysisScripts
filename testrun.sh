81730c5c958ba2e1b4ca96db047c6e9111f41979
ee12lmb <ee12lmb@leeds.ac.uk>
2015-10-22 16:38:20 +0100

    Output pdf now cropped and more input checks added

--- testrun.sh
+++ testrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo
 # test script to run J-index matlab function from command line
 
 # set up shell variables 
@@ -7,11 +7,16 @@ script=${0##*/}
 mfunc="/nfs/see-fs-01_teaching/ee12lmb/project/source/intial_scripts/jstrain"
 funcpath="/nfs/see-fs-01_teaching/ee12lmb/project/source/dev/Jstrain"
 
+echo "Running analysis script: $script"
+echo "------------------------------------"
+
 # input checks
 [[ $# -ne 2 ]] && echo "$script: testrun [ texture file ] [ output plot pdf ]" && exit 1
+[[ $2 != *.pdf ]] && echo "$script: output file should be .pdf" && exit 1 
 
 infile=$1
 outfile=$2
+outdir=$(echo $2 | rev | cut -d"/" -f2- | rev)
 
 [[ ! -f $infile ]] && echo "$script: input file does not exist, check file path" && exit 1
 [[ -f $outfile ]]  && echo "$script: output file already exists" && exit 1
@@ -19,8 +24,13 @@ outfile=$2
 echo "$script: $(date): calling matlab function: jstrain('$infile','$outfile')..."
 echo
 matlab -nodesktop -nodisplay -nosplash -r "addpath('$funcpath'); jstrain('$infile','$outfile'); exit"
-printf "done"
+echo "done"
 
+# crop pdf so plot fills page
+pdfcrop $outfile "$outdir/crop-temp.pdf" > /dev/null
+mv "$outdir/crop-temp.pdf" $outfile
+
+echo
 echo "$script: $(date): output pdf: $outfile"
 exit 0
   
